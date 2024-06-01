2b5e0ef658481d233c5bb1d453722d7e920df0a8
user1315492038 <1315492038@qq.com>
2024-06-01 17:32:39 +0800

    delete unused

--- student/urls.py
+++ student/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from django.contrib import admin
 from student import views
 
-app_name = 'student'
-
 urlpatterns = [
     path('index/',views.index),
     path('login/',views.login),
